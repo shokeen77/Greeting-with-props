868873731eba0dfe01e6855aa6a7a7a295bb73a7
Nishant <shokeennishant444@gmail.com>
2023-05-21 20:00:51 +0530

    Change

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import '../styles/App.css';
+
 import Welcome from './Welcome';
 
 const App = () => {
-  let name = "Nishant Shokeen";
+  let name = "Anand Dubey";
   return (
     <Welcome name={name} />
   )
